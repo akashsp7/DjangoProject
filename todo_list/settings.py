7d487a5584bb4fb8d5123732284e00ca68eb1f87
akashsp7 <akashsp7666@gmail.com>
2022-04-22 00:52:02 +0530

    seventh commit

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -133,4 +133,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 
 # Activate Django-Heroku
-todo_list.settings(locals())
+django_heroku.settings(locals())
